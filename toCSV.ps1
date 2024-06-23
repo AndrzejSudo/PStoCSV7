bc58f096983563e29c57370dd338a97ab22b56ee
andsko92 <jk2rlz@op.pl>
2024-06-24 00:33:17 +0200

    anothe release

--- toCSV.ps1
+++ toCSV.ps1
@@ -1,38 +1,36 @@
 
-$allVals = ''
 $cnt = 3
 $fileName = 'data.csv'
+$deli = '	'
+$flag = ''
 $pwd = '' + (Get-Location) + "\" + $fileName
 
-if (test-path $pwd) {
-    write-host "`nThere is already $fileName file in current directory"
-    write-host "By default its going to be replaced by new sets of data"
-    write-host "Move/rename old file or else you will lose its content`n"
-    read-host -prompt "Enter to proceed"
-}
-
 try {
+    if (test-path $pwd) {
+        write-host "`nThere is already '$fileName' file in current directory"
+        $flag = read-host -prompt "If you want to merge new data with old, type [m] and if you want to replace it hit [Enter]"
+        if ($flag -ne 'm' -or $flag -ne 'M') {
+                remove-item -path $fileName
+            }
+    }
+
     function get-vals {
         for ($i = 1; $i -le $cnt; $i++) {
             $input = Read-Host -Prompt "Enter value no. $i"
             $vals += $input + '	'
             if ($i -eq $cnt) {
-            $vals += "`n"
             }
         }
         return $vals
     }
 
     while ($true) {
-    
-        $allVals += get-vals
-        $allVals | out-file -filepath $fileName
-
-        $yesNo = read-host -Prompt "Press [q] to quit or Enter to type more data"
+        
+        get-vals | out-file -filepath $filename -append
+        $yesNo = read-host -Prompt "Press [q] to quit or press [Enter] to type more data"
         if ($yesNo -eq 'q' -or $yesNo -eq 'Q') {
             write-host "Data exported to $pwd"
-            $allVals = ''
-            break
+            exit
         }
         else {
             write-host "Type new set of data"
@@ -41,6 +39,13 @@ try {
     }
 }
 
+catch [System.IO.IOException] {
+    write-host "File already in use"
+}
+<#
+catch [someFutureErrors] {
+} 
+#>
 finally {
-    #write-host "`nQuitting without saving last dataset"
+    write-host "`nQuitting"
 }
